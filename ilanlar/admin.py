d9291c0435185ff07bf2a1cd7d06239354a43635
seyfettinkeskin7-hue <seyfettinkeskin7@gmail.com>
2026-04-22 21:44:03 +0300

    Ilan admin resim inline eklendi

--- ilanlar/admin.py
+++ ilanlar/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Ilan, IlanMedya, Favori, Haber
 
+class IlanMedyaInline(admin.TabularInline):
+    model = IlanMedya
+    extra = 3
+
+class IlanAdmin(admin.ModelAdmin):
+    inlines = [IlanMedyaInline]
+
 admin.site.register(Haber)
-admin.site.register(Ilan)
-admin.site.register(IlanMedya)
+admin.site.register(Ilan, IlanAdmin)
 admin.site.register(Favori)
