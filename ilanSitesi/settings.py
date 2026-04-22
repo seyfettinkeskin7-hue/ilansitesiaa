b6f9fa00301dbefcaae4fac23ff5dbc92c86cff8
seyfettinkeskin7-hue <seyfettinkeskin7@gmail.com>
2026-04-22 20:51:31 +0300

    STORAGES geri alindi

--- ilanSitesi/settings.py
+++ ilanSitesi/settings.py
@@ -94,14 +94,6 @@ import base64, json, os
 from google.oauth2 import service_account
 
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-STORAGES = {
-    "default": {
-        "BACKEND": "storages.backends.gcloud.GoogleCloudStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
-    },
-}
 GS_BUCKET_NAME = 'projem6565'
 
 _creds_b64 = os.environ.get('GS_CREDENTIALS_BASE64')
